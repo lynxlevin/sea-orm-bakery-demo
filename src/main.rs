1d99ba043941c0c6a733bfbea9778fe2c9dc3e99
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2024-07-14 14:02:06 +0900

    Code to open db connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+
+use dotenvy::dotenv;
+use futures::executor::block_on;
+use sea_orm::{Database, DbErr};
+
+async fn run() -> Result<(), DbErr> {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let db = Database::connect(database_url).await?;
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = block_on((run())) {
+        panic!("{}", err);
+    }
 }
