3f5220f2db4702968c19e66439fa8fd3a9e401c9
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2024-07-21 10:04:30 +0900

    Code to create DB

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,48 @@ use std::env;
 
 use dotenvy::dotenv;
 use futures::executor::block_on;
-use sea_orm::{Database, DbErr};
+use sea_orm::{ConnectionTrait, Database, DbBackend, DbErr, Statement};
 
 async fn run() -> Result<(), DbErr> {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let db = Database::connect(database_url).await?;
+    let db_name = env::var("DB_NAME").expect("DB_NAME must be set");
+
+    let db = Database::connect(&database_url).await?;
+
+    let db = &match db.get_database_backend() {
+        DbBackend::MySql => {
+            db.execute(Statement::from_string(
+                db.get_database_backend(),
+                format!("CREATE DATABASE IF NOT EXISTS `{}`;", db_name),
+            ))
+            .await?;
+            let url = format!("{}/{}", &database_url, db_name);
+            Database::connect(&url).await?
+        }
+        DbBackend::Postgres => {
+            db.execute(Statement::from_string(
+                db.get_database_backend(),
+                format!("DROP DATABASE IF EXISTS \"{}\";", db_name),
+            ))
+            .await?;
+            db.execute(Statement::from_string(
+                db.get_database_backend(),
+                format!("CREATE DATABASE \"{}\";", db_name),
+            ))
+            .await?;
+
+            let url = format!("{}/{}", &database_url, db_name);
+            Database::connect(&url).await?
+        }
+        DbBackend::Sqlite => db,
+    };
 
     Ok(())
 }
 
 fn main() {
-    if let Err(err) = block_on((run())) {
+    if let Err(err) = block_on(run()) {
         panic!("{}", err);
     }
 }
