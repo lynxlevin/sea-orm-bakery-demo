b556419382a08121915751e0b2fb386f61400026
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2024-07-21 10:42:09 +0900

    Add tables Bakery and Chef
    - Run `sea-orm-cli migrate up` to apply migration

--- migration/src/m20240721_000001_create_bakery_table.rs
+++ migration/src/m20240721_000001_create_bakery_table.rs
@@ -1,47 +1,45 @@
 use sea_orm_migration::prelude::*;
 
-#[derive(DeriveMigrationName)]
 pub struct Migration;
+
+impl MigrationName for Migration {
+    fn name(&self) -> &str {
+        "m20240721_000001_create_bakery_table"
+    }
+}
 
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
-                    .if_not_exists()
+                    .table(Bakery::Table)
                     .col(
-                        ColumnDef::new(Post::Id)
+                        ColumnDef::new(Bakery::Id)
                             .integer()
                             .not_null()
                             .auto_increment()
                             .primary_key(),
                     )
-                    .col(ColumnDef::new(Post::Title).string().not_null())
-                    .col(ColumnDef::new(Post::Text).string().not_null())
+                    .col(ColumnDef::new(Bakery::Name).string().not_null())
+                    .col(ColumnDef::new(Bakery::ProfitMargin).double().not_null())
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .drop_table(Table::drop().table(Bakery::Table).to_owned())
             .await
     }
 }
 
-#[derive(DeriveIden)]
-enum Post {
+#[derive(Iden)]
+pub enum Bakery {
     Table,
     Id,
-    Title,
-    Text,
+    Name,
+    ProfitMargin,
 }
--- migration/src/main.rs
+++ migration/src/main.rs
@@ -1,6 +1,10 @@
+use dotenvy::dotenv;
 use sea_orm_migration::prelude::*;
+use std::env;
 
 #[async_std::main]
 async fn main() {
+    dotenv().ok();
+    let DATABASE_URL = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     cli::run_cli(migration::Migrator).await;
 }
