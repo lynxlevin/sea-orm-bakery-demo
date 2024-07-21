42207afc035badfe120b2341d2ec1e912f3488e6
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2024-07-21 10:49:52 +0900

    sea-orm-cli automatically looks inside .env file for DATABASE_URL.

--- migration/src/main.rs
+++ migration/src/main.rs
@@ -1,10 +1,6 @@
-use dotenvy::dotenv;
 use sea_orm_migration::prelude::*;
-use std::env;
 
 #[async_std::main]
 async fn main() {
-    dotenv().ok();
-    let DATABASE_URL = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     cli::run_cli(migration::Migrator).await;
 }
